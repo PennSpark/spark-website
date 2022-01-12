200a8421d59c79339e3ebcfba462a52247de9df5
Grace Jiang <gracjiang@gmail.com>
2022-01-11 21:09:04 -0500

    add app redirects

---
+++ src/pages/join-blue/index.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import "./style.css"
+import "../../styles/global.css";
+import "../../styles/animations.css";
+
+
+const JoinBluePage = () => {
+  return (
+    <html>
+    <head>
+      <title>Join Blue | Spark</title>
+      <meta http-equiv="refresh" content="0; url='https://forms.gle/3oBuVZx3Yb7peY6N9'" />
+    </head>
+    <body>
+      <div className="redirect-container">
+        <h1>If you are not automatically redirected, please apply through the <a href="https://forms.gle/3oBuVZx3Yb7peY6N9">following form</a>.</h1>
+      </div>
+    </body>
+  </html>
+  );
+};
+
+export default JoinBluePage;
---
+++ src/pages/join-red/index.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import "./style.css"
+import "../../styles/global.css";
+import "../../styles/animations.css";
+
+
+const JoinRedPage = () => {
+  return (
+    <html>
+    <head>
+      <title>Join Red | Spark</title>
+      <meta http-equiv="refresh" content="0; url='https://forms.gle/NSmBmuauMa8BZLed7'" />
+    </head>
+    <body>
+      <div className="redirect-container">
+        <h1>If you are not automatically redirected, please apply through the <a href="https://forms.gle/NSmBmuauMa8BZLed7">following form</a>.</h1>
+      </div>
+    </body>
+  </html>
+  );
+};
+
+export default JoinRedPage;
