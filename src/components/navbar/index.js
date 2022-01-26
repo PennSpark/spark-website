9bb54e6c484d8d05ced4b2d94e04cd869c642477
Grace Jiang <gracjiang@gmail.com>
2022-01-26 17:22:41 -0500

    change navbar to hover nav

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,26 +6,39 @@ import HomeImg from "../../images/bw-logo.png";
 
 export default function Navbar() {
   return (
-    <div className="navbar-custom-container">
-      <ul className="navbar-custom">
-        <li className="navbar-custom-left-link">
+    <div className="nav-container">
+      <div className="nav-bar">
+        <div className="nav-left">
           <a href="/">
-            <img id="navbar-custom-img" src={HomeImg} />
+            <img src={HomeImg} />
           </a>
-        </li>
+        </div>
+        <div className="nav-right">
+          
+          <div className="nav-dropdown">
+            <button className="nav-dropbtn" >
+              <div className="nav-link">About</div>
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <div className="nav-dropdown-content">
+              <a className="nav-link" href="/about">About Us</a>
+              <a className="nav-link" href="http://work.pennspark.org/" target="_blank" rel="noopener noreferrer">Our Work</a>
+              <a className="nav-link" href="https://community.pennspark.org/" target="_blank" rel="noopener noreferrer">Our Community</a>
+            </div>
+          </div>
 
-        <li className="navbar-custom-right-link">
-          <a href="/join">Join</a>
-        </li>
-
-        <li className="navbar-custom-right-link">
-          <a href="/resources">Resources</a>
-        </li>
-
-        <li className="navbar-custom-right-link">
-          <a href="/about">About</a>
-        </li>
-      </ul>
+          <div className="nav-dropdown">
+            <button className="nav-dropbtn" >
+              <div className="nav-link">For Students</div>
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <div className="nav-dropdown-content">
+              <a className="nav-link" href="/resources">Resources</a>
+              <a className="nav-link" href="/join">Join Us</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
