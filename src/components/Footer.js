b6bec368fcc0a50541ab2bc744da1a60aa479d22
exyzhao <ethanxyzhao@gmail.com>
2022-07-25 19:50:38 -0500

    Create footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,47 @@ import styled from "styled-components";
 
 const FooterContainer = styled.div`
   display: flex;
+  justify-content: space-evenly;
+  padding-top: 20px;
   background-color: #eee;
+  gap: 40px;
+`
+
+const VerticalContainer = styled.div`
+
+  .subheader {
+    font-weight: bold;
+    margin-bottom: 1em;
+  }
 `
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <p>Connect with us</p>
-      <p>About us</p>
+      <VerticalContainer>
+        <p className="subheader">Connect with us</p>
+        <p>Penn Clubs</p>
+        <p>Newsletter</p>
+      </VerticalContainer>
+      <VerticalContainer>
+        <p className="subheader">About Us</p>
+        <p>About Spark</p>
+        <p>Our Community</p>
+        <p>Our Branches</p>
+      </VerticalContainer>
+      <VerticalContainer>
+        <p className="subheader">Our Work</p>
+        <p>Spring 2022</p>
+        <p>Fall 2021</p>
+        <p>Spring 2021</p>
+      </VerticalContainer>
+      <VerticalContainer>
+        <p className="subheader">Work With Us</p>
+        <p>Why Spark?</p>
+        <p>Pitch Deck</p>
+        <p>Project Application</p>
+        <p>Sponsor Us</p>
+      </VerticalContainer>
     </FooterContainer>
   )
 }
