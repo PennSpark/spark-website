8a74d982bcd5c909a1e0f135f9cfa8c6a9eb0967
Kashyap Chaturvedula <kashyapc@seas.upenn.edu>
2023-08-29 18:05:05 -0400

    swapped design and dev technical imports

--- src/components/sections/join-us/join-us.js
+++ src/components/sections/join-us/join-us.js
@@ -1,12 +1,12 @@
 import * as React from "react";
 import "../style.css";
-import FadeIn from 'react-fade-in';
+import FadeIn from "react-fade-in";
 
 import WhyJoin from "../why-join";
 import FAQ from "../faq";
 
-import DevTechnical from "./pdf/red-design-technical-assessment.pdf"
-import DesignTechnical from "./pdf/red-developer-technical-assessment.pdf"
+import DesignTechnical from "./pdf/red-design-technical-assessment.pdf";
+import DevTechnical from "./pdf/red-developer-technical-assessment.pdf";
 
 import CustomLink from "../../custom-link";
 
@@ -21,12 +21,17 @@ export default function JoinUs() {
       <br />
       <br />
       <br />
-      <div style={{ minHeight: "100vh" }} id="join-content-section" className="content-section">
+      <div
+        style={{ minHeight: "100vh" }}
+        id="join-content-section"
+        className="content-section"
+      >
         {/* <p className="larger-text" style={{ textAlign: "center" }}>
           Applications will open early in the fall. In the meantime, check out our <a href="/resources">student resources</a>.
         </p> */}
         <p className="larger-text" style={{ textAlign: "center" }}>
-          Our Fall 2023 applications are now live and due on <strong>Thursday, September 7, 2023 at 11:59PM EST</strong>!
+          Our Fall 2023 applications are now live and due on{" "}
+          <strong>Thursday, September 7, 2023 at 11:59PM EST</strong>!
         </p>
         {/* <p className="larger-text" style={{ textAlign: "center", color: "#cf6c5d" }}>
           The deadline has been extended to <strong>Friday, January 20, 2023 at 11:59PM EST</strong>!
@@ -34,14 +39,20 @@ export default function JoinUs() {
         <br />
         <br />
 
-
         <div id="join-blue" className="join-app-container">
-          <strong style={{ fontSize: "1.2rem", fontWeight: "600", color: "#577ed2" }}>Apply for Blue</strong>
+          <strong
+            style={{ fontSize: "1.2rem", fontWeight: "600", color: "#577ed2" }}
+          >
+            Apply for Blue
+          </strong>
 
           <div />
 
-
-          <a href="https://forms.gle/PHbBsbPjdENGFNm46" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://forms.gle/PHbBsbPjdENGFNm46"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <button id="blue-button">Blue Application &rarr;</button>
           </a>
         </div>
@@ -49,11 +60,19 @@ export default function JoinUs() {
         <div className="join-app-spacer" />
 
         <div id="join-red" className="join-app-container">
-          <strong style={{ fontSize: "1.2rem", fontWeight: "600", color: "#cf6c5d" }}>Apply for Red</strong>
+          <strong
+            style={{ fontSize: "1.2rem", fontWeight: "600", color: "#cf6c5d" }}
+          >
+            Apply for Red
+          </strong>
 
           <div />
 
-          <a href="https://forms.gle/uPoz6cqrMD1tpVuW6" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://forms.gle/uPoz6cqrMD1tpVuW6"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <button id="red-button">Red Application &rarr;</button>
           </a>
 
@@ -64,20 +83,30 @@ export default function JoinUs() {
           <a href={DesignTechnical} target="_blank" rel="noopener noreferrer">
             <button id="red-button">Design Assessment &rarr;</button>
           </a>
-
         </div>
 
-
         <div>
-          <p style={{ padding: "14px 18px 14px 18px", border: "2px solid #ccc", borderRadius: "10px" }}>
-            <strong style={{ fontSize: "1.2rem", fontWeight: "600" }}>General Info Session</strong>
+          <p
+            style={{
+              padding: "14px 18px 14px 18px",
+              border: "2px solid #ccc",
+              borderRadius: "10px",
+            }}
+          >
+            <strong style={{ fontSize: "1.2rem", fontWeight: "600" }}>
+              General Info Session
+            </strong>
             <div style={{ height: "4px" }} />
             Tuesday September 5th, 7-8pm EST
             <div style={{ height: "16px" }} />
             Interested in learning more about Penn Spark? We are hosting a
-            General Info Meeting on <b>Tuesday September 5th, at 7:00pm EST in Skirkanich Auditorium</b> (located in the engineering complex). Join us to
-            learn more about what we do and get your questions answered about our
-            application process!
+            General Info Meeting on{" "}
+            <b>
+              Tuesday September 5th, at 7:00pm EST in Skirkanich Auditorium
+            </b>{" "}
+            (located in the engineering complex). Join us to learn more about
+            what we do and get your questions answered about our application
+            process!
             <br />
             <br />
             {/* The password for the Zoom Recording is "<b>sparkupenn2022!</b>". */}
@@ -93,7 +122,6 @@ export default function JoinUs() {
           </a> */}
           </p>
         </div>
-
       </div>
       {/* <AppDetails /> */}
       <WhyJoin />
