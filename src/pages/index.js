f70a6c2a78a1bf01f6a19839cfb4b2ba5e2a6cf2
Kashyap Chaturvedula <kashyapc@seas.upenn.edu>
2023-09-16 14:45:44 -0400

    removed recruiter banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -99,7 +99,6 @@ const IndexPage = () => {
     <Layout title="Home | Spark 💡">
       <Navbar />
       <FadeIn>
-        <RecruitingBanner />
         <div id="header-section">
           <h1>
             we are <span id="header-spark-text">spark</span>
