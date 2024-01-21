0a3c59bf04074aa1fc127b7da05052f73d8996cc
lohpaul9 <lohpaul9@seas.upenn.edu>
2024-01-21 12:54:51 -0800

    red apps up

--- src/components/sections/join-us/join-us.js
+++ src/components/sections/join-us/join-us.js
@@ -5,12 +5,16 @@ import FadeIn from "react-fade-in";
 import WhyJoin from "../why-join";
 import FAQ from "../faq";
 
-import DesignTechnical from "./pdf/red-design-technical-assessment.pdf";
-import DevTechnical from "./pdf/red-developer-technical-assessment.pdf";
+
 
 import CustomLink from "../../custom-link";
+import RedApp from "./red-app";
 
 export default function JoinUs() {
+  const currentDate = new Date();
+  const applicationDeadline = new Date("2024-01-21T23:59:00-05:00"); // Set the application deadline date and time
+
+  const shouldDisplayRedApp = currentDate > applicationDeadline;
   return (
     <FadeIn>
       <div id="header-section">
@@ -22,25 +26,33 @@ export default function JoinUs() {
       <br />
       <br />
       <div
-        style={{ minHeight: "100vh" }}
+        style={{ minHeight: "60vh" }}
         id="join-content-section"
         className="content-section"
       >
-        {/* <p className="larger-text" style={{ textAlign: "center" }}>
-          Applications will open early in the fall. In the meantime, check out our <a href="/resources">student resources</a>.
-        </p> */}
-        <p className="larger-text" style={{ textAlign: "center" }}>
-          Our Fall 2023 applications are now live and blue apps are due on{" "}
-          <strong>Thursday, September 7, 2023 at 11:59PM EST</strong>. <i>Red apps have been extended to
-            <strong> Friday, September 8, 2023 at noon EST</strong>!</i>
-        </p>
+        {
+          !shouldDisplayRedApp ? (
+            <p className="larger-text" style={{ textAlign: "center" }}>
+              Applications for Spring 2024 coming very soon - stay tuned!
+              <br />
+              Keep scrolling to read more about our application process.
+            </p>
+          ) : (
+            <p className="larger-text" style={{ textAlign: "center" }}>
+              Our Spring 2024 applications are now live and red apps are due on {""}
+              <strong>Sunday, January 28, 2024 at 11:59PM EST</strong>. <i>Note: We will ONLY be accepting
+                red apps in the Spring due to the blue being a year-long curriculum.</i>
+            </p>
+          )
+        }
+
         {/* <p className="larger-text" style={{ textAlign: "center", color: "#cf6c5d" }}>
           The deadline has been extended to <strong>Friday, January 20, 2023 at 11:59PM EST</strong>!
         </p> */}
         <br />
         <br />
 
-        <div id="join-blue" className="join-app-container">
+        {/* <div id="join-blue" className="join-app-container">
           <strong
             style={{ fontSize: "1.2rem", fontWeight: "600", color: "#577ed2" }}
           >
@@ -56,37 +68,21 @@ export default function JoinUs() {
           >
             <button id="blue-button">Blue Application &rarr;</button>
           </a>
-        </div>
+        </div> */}
 
-        <div className="join-app-spacer" />
+        {
+          shouldDisplayRedApp && ( // Only display the Red app if the current date is after the application deadline
+            <RedApp />
+          )
+        }
 
-        <div id="join-red" className="join-app-container">
-          <strong
-            style={{ fontSize: "1.2rem", fontWeight: "600", color: "#cf6c5d" }}
-          >
-            Apply for Red
-          </strong>
+        {/* <RedApp /> */}
 
-          <div />
+        {/* <div className="join-app-spacer" /> */}
 
-          <a
-            href="https://forms.gle/uPoz6cqrMD1tpVuW6"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button id="red-button">Red Application &rarr;</button>
-          </a>
 
-          <a href={DevTechnical} target="_blank" rel="noopener noreferrer">
-            <button id="red-button">Development Assessment &rarr;</button>
-          </a>
 
-          <a href={DesignTechnical} target="_blank" rel="noopener noreferrer">
-            <button id="red-button">Design Assessment &rarr;</button>
-          </a>
-        </div>
-
-        <div>
+        {/* <div>
           <p
             style={{
               padding: "14px 18px 14px 18px",
@@ -110,19 +106,9 @@ export default function JoinUs() {
             our application process!
             <br />
             <br />
-            {/* The password for the Zoom Recording is "<b>sparkupenn2022!</b>". */}
             <div style={{ height: "24px" }} />
-            {/* <a href="https://fb.me/e/aq7DCZAlU" target="_blank" rel="noopener noreferrer">
-            <button id="gim-btn">Facebook Event &rarr;</button>
-          </a>
-          <a href="https://upenn.zoom.us/rec/share/nIqDLujX_XgD7V1kFyAXC0dxAwWvlxyLLHkkfy5VPKZxSfwG2KIl2--OF0O6HJui.g8HJUEOtljjTjLj3?startTime=1662505185000" target="_blank" rel="noopener noreferrer">
-            <button id="gim-btn">Zoom Recording &rarr;</button>
-          </a>
-          <a href="https://docs.google.com/presentation/d/1lTD7PwAniSwVodA-no6qjQauxp8Gib07v8W07Kl3uDE/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
-            <button id="gim-btn">Meeting Slides &rarr;</button>
-          </a> */}
           </p>
-        </div>
+        </div> */}
       </div>
       {/* <AppDetails /> */}
       <WhyJoin />
---
+++ src/components/sections/join-us/red-app.js
@@ -0,0 +1,34 @@
+import * as React from "react";
+import "../style.css";
+import DesignTechnical from "./pdf/red-design-technical-assessment.pdf";
+import DevTechnical from "./pdf/red-developer-technical-assessment.pdf";
+
+export default function RedApp() {
+  return (
+    <div id="join-red" className="join-app-container">
+      <strong
+        style={{ fontSize: "1.2rem", fontWeight: "600", color: "#cf6c5d" }}
+      >
+        Apply for Red
+      </strong>
+
+      <div />
+
+      <a
+        href="https://forms.gle/eqZsdm4SgK8C1gnT6"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button id="red-button">Red Application &rarr;</button>
+      </a>
+
+      <a href={DevTechnical} target="_blank" rel="noopener noreferrer">
+        <button id="red-button">Development Assessment &rarr;</button>
+      </a>
+
+      <a href={DesignTechnical} target="_blank" rel="noopener noreferrer">
+        <button id="red-button">Design Assessment &rarr;</button>
+      </a>
+    </div>
+  )
+}
