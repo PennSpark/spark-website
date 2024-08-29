a86db081e688112833f028d3d61dbe79a400b150
nick-cirillo <77818146+nick-cirillo@users.noreply.github.com>
2024-08-28 21:44:13 -0400

    color

--- src/components/sections/join-us/interest-form.js
+++ src/components/sections/join-us/interest-form.js
@@ -5,13 +5,13 @@ export default function InterestForm() {
   return (
     <div id="interest-form" className="join-app-container">
       <strong
-        style={{ fontSize: "1.2rem", fontWeight: "600", color: "linear-gradient(90deg, #e66465, #9198e5)" }}
+        style={{ fontSize: "1.2rem", fontWeight: "600", color: "black" }}
       >
         General Information Meeting
       </strong>
 
       <p
-        style={{ fontSize: "1rem", fontWeight: "400", color: "#9198e5" }}
+        style={{ fontSize: "1rem", fontWeight: "400", color: "black" }}
       >
         We're hosting a GIM at Berger Auditorium at 7PM on 9/3. Join us to meet the team and learn about what we do! Sign up so you won't miss a thing.
       </p>
