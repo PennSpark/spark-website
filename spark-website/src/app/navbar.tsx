265b07a3e170ae44037aa8a558498f3143529424
Bean-Github <kevinw11235@gmail.com>
2025-10-24 02:35:46 -0400

    finished task

--- spark-website/src/app/navbar.tsx
+++ spark-website/src/app/navbar.tsx
@@ -1,21 +1,27 @@
 import Image from "next/image";
 
 export default function Navbar() {
+  const linkStyle = "text-gray-800 hover:text-gray-400";
+
   return (
     // navbar
     <div className="w-full h-16 flex items-center px-8 bg-linear-to-r from-gray-30 to-gray-400 shadow-md">
-      <h1 className="ml-4 text-lg font-semibold">Penn Spark header!</h1>
+      <h1 className="ml-4 text-lg font-semibold">
+        <a href="/" className={linkStyle}>
+          Penn Spark header!
+        </a>
+      </h1>
 
       {/* links to different pages */}
       <div className="ml-auto flex gap-4">
-        <a href="/" className="hover:underline">
-          Home
+        <a href="/about" className={linkStyle}>
+          About Us
         </a>
-        <a href="/about" className="hover:underline">
-          About
+        <a href="/work" className={linkStyle}>
+          Our Work
         </a>
-        <a href="/projects" className="hover:underline">
-          Projects
+        <a href="/community" className={linkStyle}>
+          Our Community
         </a>
       </div>
     </div>
