7011aa692062a4a3687200d0a6fad3995b0db467
violinmeiling <violinmeiling@gmail.com>
2026-01-13 16:38:46 -0500

    Update interest-form.js
    s26 update interest form

--- src/components/sections/join-us/interest-form.js
+++ src/components/sections/join-us/interest-form.js
@@ -7,19 +7,19 @@ export default function InterestForm() {
       <strong
         style={{ fontSize: "1.2rem", fontWeight: "bold", color: "black" }}
       >
-        VISIT US AT SAC FAIR
+        INTEREST FORM
       </strong>
 
       <p
         style={{ fontSize: "1rem", fontWeight: "600", color: "black" }}
       >
-        We'll be at SAC Fair on Wednesday, August 27th from 4pm-7pm on Locust! Join us to meet the team and learn about what we do! Sign up so you won't miss a thing.
+        Our General Information Meeting is on Jan 22 at 7pm! Location coming soon. Join us to meet the team and learn about what we do. Sign up for our interest form and follow us on Instagram at @pennspark so you won't miss a thing.
       </p>
 
       <div />
 
       <a
-        href="https://forms.gle/5FtisYh8VoQQRs5h8"
+        href="https://forms.gle/F1jUPKgVgbbtSWfi6"
         target="_blank"
         rel="noopener noreferrer"
       >
