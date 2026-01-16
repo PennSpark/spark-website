afe7e17dbaf9a842ea536611b60fbc64e5a82fc0
violinmeiling <violinmeiling@gmail.com>
2026-01-16 00:26:40 -0500

    sp26 GIM location

--- src/components/sections/join-us/interest-form.js
+++ src/components/sections/join-us/interest-form.js
@@ -13,7 +13,7 @@ export default function InterestForm() {
       <p
         style={{ fontSize: "1rem", fontWeight: "600", color: "black" }}
       >
-        Our General Information Meeting is on Jan 22 at 7pm! Location coming soon. Join us to meet the team and learn about what we do. Sign up for our interest form and follow us on Instagram at @pennspark so you won't miss a thing.
+        Our General Information Meeting is on Jan 22 at 7pm in JMHH F70! Join us to meet the team and learn about what we do. Sign up for our interest form and follow us on Instagram at @pennspark so you won't miss a thing.
       </p>
 
       <div />
