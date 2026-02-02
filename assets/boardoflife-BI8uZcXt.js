81600a42618e5092a0b46734331a99fc5a6244f8
kimestelle <137699667+kimestelle@users.noreply.github.com>
2026-02-02 15:15:36 -0500

    Updates

--- assets/boardoflife-BI8uZcXt.js
+++
@@ -1,6 +0,0 @@
-import{j as e,p as n}from"./index-CCX72tvS.js";import r from"./ProjectLayout-Bgb0-LsF.js";const t="/fall23/boardoflife",o=n[t];if(!o)throw new Error(`Project not found in projectByKey for pageKey: ${t}`);const s=[e.jsx("a",{style:{fontSize:"24px"},href:`https://drive.google.com/file/d/1Ct_MiSQgFq6aSCo0qTyFwK7h4hncQZ9x/view?resourcekey
-`,children:"Slides here!"},"v1")],i=[{title:"Background",content:[`Meeting management platform equipped with AI coaching. Target users: leaders, CEOs, decision makers. User selects AIs to converse with and get feedback on meetings. Personalization: use user’s background and experiences in generating personalized feedback
-
-
-`]},{title:"Timeline",content:[]},{title:"Challenges",content:[]},{title:"Takeaways",content:[]},{title:"Looking Forward",content:[]}];function d(){return e.jsx(r,{project:o,demoUrl:`https://docs.google.com/presentation/d/1jJsiyqqH1yEOyvhShWdY2nrE6nWcAVf0gSod4fk9SYc/edit?usp=sharing#slide=id.g1ec68889e17_46_0
-`,sourcecodeUrl:"",demoVisuals:s,sections:i})}export{d as default,t as pageKey};
