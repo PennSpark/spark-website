6b1b77d890d7c695a5f5d56c9bd1ad5f20e6df44
kimestelle <137699667+kimestelle@users.noreply.github.com>
2026-02-02 16:07:59 -0500

    Updates

--- assets/sparkify-zUog85NJ.js
+++
@@ -1,7 +0,0 @@
-import{j as e,p as n}from"./index-gERmV2YS.js";import r from"./ProjectLayout-CFRqOwCY.js";const o="/fall23/sparkify",t=n[o];if(!t)throw new Error(`Project not found in projectByKey for pageKey: ${o}`);const s=[e.jsx("a",{style:{fontSize:"24px"},href:`https://docs.google.com/presentation/d/1jJsiyqqH1yEOyvhShWdY2nrE6nWcAVf0gSod4fk9SYc/edit?usp=sharing#slide=id.g1ec68889e17_27_82
-`,children:"Slides here!"},"v1")],i=[{title:"Background",content:[`We wanted to make a moodboard for music by visualizing your music taste and creating shareable experiences. Our designers broke down the process of creating a collage, and our devs built an algorithm based on the process
-
-
-
-`]},{title:"Timeline",content:[]},{title:"Challenges",content:[]},{title:"Takeaways",content:[]},{title:"Looking Forward",content:[]}];function d(){return e.jsx(r,{project:t,demoUrl:`https://docs.google.com/presentation/d/1jJsiyqqH1yEOyvhShWdY2nrE6nWcAVf0gSod4fk9SYc/edit?usp=sharing#slide=id.g1ec68889e17_27_82
-`,sourcecodeUrl:"",demoVisuals:s,sections:i})}export{d as default,o as pageKey};
