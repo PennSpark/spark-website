fa9276de0f2d4cc70bd86d0e7505cc74c7cad609
kimestelle <137699667+kimestelle@users.noreply.github.com>
2026-02-04 20:44:04 -0500

    Updates

--- assets/socialcalendar-C40dGzPd.js
+++
@@ -1,7 +0,0 @@
-import{j as e,p as n}from"./index-CkNCIQki.js";import r from"./ProjectLayout-DeXaxWKr.js";const t="/spring24/socialcalendar",o=n[t];if(!o)throw new Error(`Project not found in projectByKey for pageKey: ${t}`);const s=[e.jsx("a",{style:{fontSize:"24px"},href:`https://youtu.be/tV9XxxSI2pg
-`,children:"Demo here!"},"v1")],a=[{title:"Background",content:[`A convenient overview of where to eat, what to buy, and where to have fun around campus. Assist social organizations, stores, restaurants in gaining more visibility. Daily promo news 
-& speedy ideas for
-weekend fun.
-
-`]},{title:"Timeline",content:[]},{title:"Challenges",content:[]},{title:"Takeaways",content:[]},{title:"Looking Forward",content:[]}];function l(){return e.jsx(r,{project:o,demoUrl:`https://youtu.be/tV9XxxSI2pg
-`,sourcecodeUrl:"",demoVisuals:s,sections:a})}export{l as default,t as pageKey};
