44f01d7b16de2878e174e84fe13652d58a3dd2fd
kimestelle <137699667+kimestelle@users.noreply.github.com>
2026-02-04 19:48:50 -0500

    deploy in root directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/spark-website/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
