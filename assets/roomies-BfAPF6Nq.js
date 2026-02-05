8e366bdbdcea6f1948c86a48685dd0e1863ca77e
kimestelle <137699667+kimestelle@users.noreply.github.com>
2026-02-04 20:27:06 -0500

    Updates

---
+++ assets/roomies-BfAPF6Nq.js
@@ -0,0 +1,10 @@
+import{j as e,p as n}from"./index-CkNCIQki.js";import s from"./ProjectLayout-DeXaxWKr.js";const o="/spring24/roomies",t=n[o];if(!t)throw new Error(`Project not found in projectByKey for pageKey: ${o}`);const i=[e.jsx("a",{style:{fontSize:"24px"},href:`https://docs.google.com/presentation/d/1_t3blmXNCFUPB0ar9ID9Q09dHste4U0JcMnTUgmOIEk/edit?usp=sharing#slide=id.g1ec67223244_1_0
+`,children:"Slides here!"},"v1")],r=[{title:"Background",content:[`A convenient overview of where to eat, what to buy, and where to have fun around campus. Assist social organizations, stores, restaurants in gaining more visibility. Daily promo news 
+& speedy ideas forAllow for a fun & easy way to co-exist!
+
+Core functionalities include: recurring tasks w/ random assignments, allow special messages for roomies, earn points & compete on a leaderboard, manage roomies (kick or invite)
+
+weekend fun.
+
+`]},{title:"Timeline",content:[]},{title:"Challenges",content:[]},{title:"Takeaways",content:[]},{title:"Looking Forward",content:[]}];function l(){return e.jsx(s,{project:t,demoUrl:`https://docs.google.com/presentation/d/1_t3blmXNCFUPB0ar9ID9Q09dHste4U0JcMnTUgmOIEk/edit?usp=sharing#slide=id.g1ec67223244_1_0
+`,sourcecodeUrl:"",demoVisuals:i,sections:r})}export{l as default,o as pageKey};
