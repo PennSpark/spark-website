fc3679dd8759be135f1eb8005025eac75541a8ac
kimestelle <137699667+kimestelle@users.noreply.github.com>
2026-02-05 23:56:05 -0500

    Updates

---
+++ assets/sparkoflife-BC95Rm_D.js
@@ -0,0 +1,8 @@
+import{j as e,p as n}from"./index-DnKFjSvw.js";import r from"./ProjectLayout-B_8LvCIE.js";const t="/fall23/sparkoflife",o=n[t];if(!o)throw new Error(`Project not found in projectByKey for pageKey: ${t}`);const i=[e.jsx("a",{style:{fontSize:"24px"},href:`https://www.youtube.com/watch?v=jmo1LZdjwf0&embeds_referring_euri=https%3A%2F%2Fdocs.google.com%2F&embeds_referring_origin=https%3A%2F%2Fdocs.google.com&source_ve_path=Mjg2NjY&themeRefresh=1
+`,children:"Slides here!"},"v1")],s=[{title:"Background",content:[`Our team wanted to put together an exhibit capturing the different stages of life through interesting digital exhibits. We’ve created four different digital artifacts, each meant to capture a different stage of life, starting from infancy and going to adulthood. All of these will be in display in the side room after the presentation, so please go check them out!
+
+
+
+
+`]},{title:"Timeline",content:[]},{title:"Challenges",content:[]},{title:"Takeaways",content:[]},{title:"Looking Forward",content:[]}];function f(){return e.jsx(r,{project:o,demoUrl:`https://docs.google.com/presentation/d/1jJsiyqqH1yEOyvhShWdY2nrE6nWcAVf0gSod4fk9SYc/edit?usp=sharing#slide=id.g1ec680f5ed9_0_0
+`,sourcecodeUrl:"",demoVisuals:i,sections:s})}export{f as default,t as pageKey};
